8b6471ffe569058bf2135503a4608c65da14640c
Daniel Purrington <trex1883@gmail.com>
2019-11-04 20:08:21 -0700

    adding Http

--- src/components/Yellow.vue
+++ src/components/Yellow.vue
@@ -1,19 +1,46 @@
 <template>
-    <div>
-        <slot></slot>
+    <div class="container">
+       <div class="row">
+           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+               <h1>Http</h1>
+               <div class="form-group">
+                   <label>UserName</label>
+                   <input type="text" class="form-control" v-model="user.username">
+               </div>
+                <div class="form-group">
+                   <label>Mail</label>
+                   <input type="text" class="form-control" v-model="user.email">
+               </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+           </div>
+       </div>
     </div>
 </template>
 
 <script>
-
+ export default {
+     data() {
+         return {
+            user: {
+                username: '',
+                email: ''
+            }
+         };
+     },
+     methods: {
+         submit() {
+            //  console.log(user);
+         }
+     }
+ }
 </script>
 
 <style scoped>
-    div {
+    /* div {
         border: 1px solid yellow;
         background-color: lightyellow;
         padding: 30px;
         margin: 20px auto;
         text-align: center
-    }
+    } */
 </style>
