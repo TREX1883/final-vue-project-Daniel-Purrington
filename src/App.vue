449591bc602835a24789911e81553e1af6897bd7
Daniel Purrington <trex1883@gmail.com>
2019-11-15 22:14:35 -0700

    add v-app

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,53 @@
 
 <template>
-    <!-- <div class="container">
-    <div class="col-lg-12">
-    <v-toolbar-items class="white">
-                <v-btn class="white" @click="selectedComponent = 'appLogin'" text>Login</v-btn>
-                <v-btn class="white" @click="selectedComponent = 'appTrading'" text>MTG Trading</v-btn>
-                <v-btn class="white" @click="selectedComponent = 'appBlue'" text>Load Blue Template</v-btn>
-                <v-btn class="white" @click="selectedComponent = 'appRed'" text>Load Red Template</v-btn>  
-                <hr>
-                <component :is="selectedComponent">
-                    <p>This is the Content</p>
-                </component>
-      </v-toolbar-items>
-    </div>
-  </div> -->
-   <!-- class="col-lg-12" -->
-    <div class="container">
-        <div>
+    <v-app>
+        <v-content>
+            <v-container>
+        <v-parallax 
+        src="https://images2.alphacoders.com/571/571767.jpg" 
+        height="700" 
+        justify="center" 
+        align="center">
+            <h1 class="text-center">MTG</h1>
+            <h2 class="text-center">Trading</h2>
+            <app-footer></app-footer>
+        </v-parallax>
+    <!-- <div class="container"> -->
+        <!-- <div>
             <v-btn class="btn" @click="selectedComponent = 'appLogin'">Login</v-btn>
             <v-btn class="btn" @click="selectedComponent = 'appTrading'">MTG Trading</v-btn>
             <v-btn class="btn" @click="selectedComponent = 'appBlue'">Load Blue Template</v-btn>
             <v-btn class="btn" @click="selectedComponent = 'appRed'">Load Red Template</v-btn>  
-                <!-- <hr> -->
+
             <component :is="selectedComponent">
                 <p>This is the Content</p>
             </component>
-        </div>
-
+        </div> -->
         <app-footer></app-footer>
-    </div>
+    <!-- </div> -->
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-    import Login from './components/Login.vue';
-    import Trading from './components/Trading.vue';
-    import Blue from './components/Blue.vue';
-    import Red from './components/Red.vue';
+    // import Login from './components/Login.vue';
+    // import Trading from './components/Trading.vue';
+    // import Blue from './components/Blue.vue';
+    // import Red from './components/Red.vue';
     import Footer from './components/Footer.vue';
 
     export default {
-        data: () => {
-          return {
-              selectedComponent: 'appLogin',
-          }
-        },
+    //     data: () => {
+    //       return {
+    //           selectedComponent: 'appLogin',
+    //       }
+    //     },
         components: {
-            appLogin: Login,
-            appTrading: Trading,
-            appBlue: Blue,
-            appRed: Red,
+    //         appLogin: Login,
+    //         appTrading: Trading,
+    //         appBlue: Blue,
+    //         appRed: Red,
             'app-footer': Footer
         }
     }
@@ -65,11 +64,11 @@
     } */
     .container {
     color: white;
-    background-color: black;
+    /* background-color: black; */
     /* width: 100%; */
     /* height: 100%; */
     /* background-image:url(https://images2.alphacoders.com/571/571767.jpg); */
-    background-size: cover;
+    /* background-size: cover; */
     /* background-attachment:fixed; */
     /* margin: 0; */
     /* padding: 0; */
