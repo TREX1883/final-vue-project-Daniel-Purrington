e3240c125a79c0186c2f1649456d825f018ea673
Daniel Purrington <trex1883@gmail.com>
2019-12-02 22:04:16 -0700

    add vuex for store

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { store } from './store/store'
 // import Vuetify from 'vuetify/lib';
 
 Vue.use(VueResource);
 Vue.config.productionTip = false;
-Vue.http.options.root = 'https://final-project-vue.firebaseio.com/data.json';
+// Vue.http.options.root = 'https://final-project-vue.firebaseio.com/data.json';
+Vue.http.options.root = 'https://project-vue-65b6f.firebaseio.com/data.json';
+// https://project-vue-65b6f.firebaseio.com/
 // Vue.http.interceptors.push((request, next) => {
 //     if (request.method == 'POST') {
 //       request.method = 'PUT';
@@ -18,6 +21,7 @@ Vue.http.options.root = 'https://final-project-vue.firebaseio.com/data.json';
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
 
