1e5f8c8000cc8948e7726c9655030fb731f0979f
Daniel Purrington <trex1883@gmail.com>
2022-02-04 17:19:45 -0700

    fixing issue on graphql.vue

--- src/components/graphql.vue
+++ src/components/graphql.vue
@@ -2,13 +2,46 @@
   <v-app>
     <v-content>
       <h1>GraphQL2</h1>
-      <div>{{ allMtgs }} test </div>
-      
+      <div>{{ allMtgs }} </div>
+      <p>{{ allMtgs.name }}</p>
     </v-content>
   </v-app>
 </template>
 
- <script>
+<template>
+  <!-- Apollo watched Graphql query -->
+  <ApolloQuery :query="require('../graphql/allMTGs.gql')"
+   :variables="{ searchString }">
+    <template v-slot="{ result: { loading, error, data } }">
+      <!-- Loading -->
+      <div v-if="loading" class="loading apollo">Loading...</div>
+
+      <!-- Error -->
+      <div v-else-if="error" class="error apollo">An error occured</div>
+
+      <!-- Result -->
+      <div v-else-if="data" class="result apollo">
+        <v-row>
+          <v-col cols="4" v-for="(list, i) in data.courses" :key="i">
+            <v-card class="mx-auto" max-width="350">
+              <v-card-text>
+                <div>{{ list.name }}</div>    
+                <div class="text--primary">
+                  {{ list.text }}
+                </div>
+              </v-card-text>          
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+
+      <!-- No result -->
+      <div v-else class="no-result apollo">No result</div>
+    </template>
+  </ApolloQuery>
+</template>
+
+<script>
 import gql from 'graphql-tag'
 
 export default {
@@ -35,3 +68,4 @@ export default {
   background-color: black;
 }
 </style>
+
